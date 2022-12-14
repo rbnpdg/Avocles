154c754230d634aa03db2bb3c6ceb0c0fedcfd8a
Adiskabayu <112401267+Adiskabayu@users.noreply.github.com>
2022-12-14 21:15:39 +0700

    Update Avocles.cpp

--- Avocles.cpp
+++ Avocles.cpp
@@ -1,26 +1,66 @@
+#include <dos.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <time.h>
 
-#include <iostream>
-#include <conio.h>
+#define lebarlayar 100
+#define tinggilayar 26
+#define lebarwindow 75
+#define lebarmenu 30
+#define GAP_SIZE 7
+#define PIPE_DIF 45
+
 using namespace std;
+ 
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD CursorPosition;
 
-void gameover() {
-	system("cls");
-	cout << endl;
-	cout << "\t\t__________________________" << endl;
-	cout << "\t\t|        Game Over       |" << endl;
-	cout << "\t\t|________________________|" << endl << endl;
-	cout << "\tTekan tombol sembarang untuk kembali ke menu!";
-	getch();
+int pipa[3];
+int lubang[3];
+int pipeFlag[3];
+char burung[2][6] = { '/','-','-','o','\\',' ',
+					'|','_','_','_',' ','>' };
+int birdPos = 6;
+int skor = 0;
+
+void gotoxy(int x, int y) {
+	CursorPosition.X = x;
+	CursorPosition.Y = y;
+	SetConsoleCursorPosition(console, CursorPosition);
 }
 
-int main() {
-	gameover();
+void setcursor(bool visible, DWORD size) 
+{
+	if(size == 0)
+		size = 20;	
 	
-	system("cls");
-	cout<<"  ________________________ \n"; 
-	cout<<" |        Avocles!        | \n"; 
-	cout<<" |________________________| \n";
-	cout<<"Mulai[1]\n";
-	cout<<"Keluar[2]\n";	 
-	cout<<"Masukkan pilihan : \n";
+	CONSOLE_CURSOR_INFO lpCursor;	
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = size;
+	SetConsoleCursorInfo(console,&lpCursor);
+}
+
+void border(){ 
+	
+	for(int i = 0; i < lebarlayar; i++) {
+		gotoxy(i, 0); 
+		cout << "-";
+		gotoxy(i, tinggilayar); 
+		cout << "-";
+	}
+	for(int i = 0; i < tinggilayar; i++) {
+		gotoxy(0, i); 
+		cout << "|";
+		gotoxy(lebarlayar,i); 
+		cout << "|";
+	}
+	for(int i = 0; i < tinggilayar; i++) {
+		gotoxy(lebarwindow,i); 
+		cout << "|";
+	}
+}
+
+void tampilPipa(int ind) {
+	lubang[ind] = 3 + rand()%14; 
 }
