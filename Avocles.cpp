78dfb27350b32ea02a4bde1e38c64596088e4a70
liraseptiyani <112398818+liraseptiyani@users.noreply.github.com>
2022-12-14 19:39:17 +0700

    Update Avocles.cpp

--- Avocles.cpp
+++ Avocles.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void gameover() {
+	system("cls");
+	cout << endl;
+	cout << "\t\t__________________________" << endl;
+	cout << "\t\t|        Game Over       |" << endl;
+	cout << "\t\t|________________________|" << endl << endl;
+	cout << "\tTekan tombol sembarang untuk kembali ke menu!";
+	getch();
+}
+
+int main() {
+	gameover();
+	
+	system("cls");
+	cout<<"  ________________________ \n"; 
+	cout<<" |        Avocles!        | \n"; 
+	cout<<" |________________________| \n";
+	cout<<"Mulai[1]\n";
+	cout<<"Keluar[2]\n";	 
+	cout<<"Masukkan pilihan : \n";
+}
